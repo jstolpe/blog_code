b105dc19ca0c17e51d4e0cca9aca064c13b739dd
jstolpe <stolpe.justin@gmail.com>
2019-01-27 17:28:51 -0600

    updates

--- faceboo_login_php/config.php
+++ faceboo_login_php/config.php
@@ -1,6 +1,6 @@
 <?php
 	// your app id goes here
-	define( 'MY_FB_APP_ID', '2266868256860115' );
+	define( 'MY_FB_APP_ID', 'YOUR-FB-APP-ID' );
 
 	// place our app secret here
 	define( 'MY_FB_APP_SECRET', 'YOUR-FB-APP-SECRET' );
