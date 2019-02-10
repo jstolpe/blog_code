8f99580f0cd0f340ded31f592c1ae97154bd7513
jstolpe <stolpe.justin@gmail.com>
2019-02-10 13:36:44 -0600

    adding fixed navigation

---
+++ fixed_navigation_bar/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- title of our page -->
+		<title>Fixed Navigation Bar with HTML and CSS</title>
+
+		<!-- need this so everything looks good on mobile devices -->
+		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+
+		<!-- include our styles -->
+		<link href="styles.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<!-- this is the fixed navigation bar -->
+		<div class="fixed-navigation-bar">
+			<div class="nav-bar-container">
+				<div class="options-container">
+					LOGIN | MENU
+				</div>
+			</div>
+		</div>
+
+		<!-- this is our site content that scrolls -->
+		<div class="site-content">
+			<div class="content-centered">
+				<div class="section">
+					<div class="section-padding">
+						<h2>Fixed Navigation Bar with HTML and CSS!</h2>
+						<p>
+							This webpage is an example of how to create a fixed navigation bar with HTML and CSS. The fixed navigation bar will stick to the top of the webpage on desktop and mobile devices when the webpage is scrolled down. This webpage is an example of how to create a fixed navigation bar with HTML and CSS. The fixed navigation bar will stick to the top of the webpage on desktop and mobile devices when the webpage is scrolled down. This webpage is an example of how to create a fixed navigation bar with HTML and CSS. The fixed navigation bar will stick to the top of the webpage on desktop and mobile devices when the webpage is scrolled down. This webpage is an example of how to create a fixed navigation bar with HTML and CSS. The fixed navigation bar will stick to the top of the webpage on desktop and mobile devices when the webpage is scrolled down. This webpage is an example of how to create a fixed navigation bar with HTML and CSS. The fixed navigation bar will stick to the top of the webpage on desktop and mobile devices when the webpage is scrolled down.
+						</p>
+					</div>
+				</div>
+				<div class="section">
+					<div class="section-padding">
+						<h2>Scroll Down!</h2>
+						<p>
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+						</p>
+					</div>
+				</div>
+				<div class="section">
+					<div class="section-padding">
+						<h2>Scroll Down!</h2>
+						<p>
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+						</p>
+					</div>
+				</div>
+				<div class="section">
+					<div class="section-padding">
+						<h2>Scroll Down!</h2>
+						<p>
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+						</p>
+					</div>
+				</div>
+				<div class="section">
+					<div class="section-padding">
+						<h2>Scroll Down!</h2>
+						<p>
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+						</p>
+					</div>
+				</div>
+				<div class="section">
+					<div class="section-padding">
+						<h2>Scroll Down!</h2>
+						<p>
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+						</p>
+					</div>
+				</div>
+				<div class="section">
+					<div class="section-padding">
+						<h2>Scroll Down!</h2>
+						<p>
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+						</p>
+					</div>
+				</div>
+				<div class="section">
+					<div class="section-padding">
+						<h2>Scroll Down!</h2>
+						<p>
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+						</p>
+					</div>
+				</div>
+				<div class="section">
+					<div class="section-padding">
+						<h2>Scroll Down!</h2>
+						<p>
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+						</p>
+					</div>
+				</div>
+				<div class="section">
+					<div class="section-padding">
+						<h2>Scroll Down!</h2>
+						<p>
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+						</p>
+					</div>
+				</div>
+				<div class="section">
+					<div class="section-padding">
+						<h2>Scroll Down!</h2>
+						<p>
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page! 
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+							If you scroll down, you will notice the fixed navigation bar stays fixed to the top of the page!
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
